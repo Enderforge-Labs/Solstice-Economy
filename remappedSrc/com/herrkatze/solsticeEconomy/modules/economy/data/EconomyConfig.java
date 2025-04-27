53f0a59f109d919a56d1b72144c54163295f8b92
scmcgowen <scmcgowen@gmail.com>
2025-04-27 16:00:42 -0700

    Remove remappedSrc

--- remappedSrc/com/herrkatze/solsticeEconomy/modules/economy/data/EconomyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.herrkatze.solsticeEconomy.modules.economy.data;
-
-import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-
-@ConfigSerializable
-public class EconomyConfig {
-
-
-}
--- remappedSrc/com/herrkatze/solsticeEconomy/modules/economy/data/EconomyPlayerData.java
+++
@@ -1,8 +0,0 @@
-package com.herrkatze.solsticeEconomy.modules.economy.data;
-
-import org.jetbrains.annotations.Nullable;
-
-public class EconomyPlayerData {
-    public long balance = 0;
-    public @Nullable String key = null; // License key to be used by the CC api for economy.
-}
